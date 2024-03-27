1b372ac8139b3eb76e61882324d702ca736e672f
Daniel Write <daniel@writefamily.com>
2024-03-27 02:46:31 -0400

    good friday tenebrae

--- horarium/lit-calendar.js
+++ horarium/lit-calendar.js
@@ -399,6 +399,84 @@ function annotateTemporalMetadata(metadata) { // attach hour information
 		}
 	}
 
+	//good friday
+	metadata.passion[2].friday.hours = {
+		Vigils: {
+			order: 'tenebrae/order.lit',
+			psalter: 'tenebrae/good-friday.lit',
+			propers: mergeDeep(lessons('propers/triduum/friday/tenebrae/'), {
+				benedictus: 'propers/triduum/friday/tenebrae/benedictus.lit',
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+				'universal-gloria-disabled': true
+			})
+		},
+
+		Lauds: {
+			order: 'tenebrae/order.lit',
+			psalter: 'tenebrae/good-friday.lit',
+			propers: mergeDeep(lessons('propers/triduum/friday/tenebrae/'), {
+				benedictus: 'propers/triduum/friday/tenebrae/benedictus.lit',
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+				'universal-gloria-disabled': true
+			})
+		},
+
+		Prime: {
+			order: 'terce/triduum-order.lit',
+			psalter: 'prime/triduum.lit',
+			propers: {
+				'universal-gloria-disabled': true,
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+			}
+		},
+
+		Terce: {
+			order: 'terce/triduum-order.lit',
+			psalter: 'terce/triduum.lit',
+			propers: {
+				'universal-gloria-disabled': true,
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+			}
+		},
+
+		Sext: {
+			order: 'terce/triduum-order.lit',
+			psalter: 'sext/triduum.lit',
+			propers: {
+				'universal-gloria-disabled': true,
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+			}
+		},
+
+		None: {
+			order: 'terce/triduum-order.lit',
+			psalter: 'none/triduum.lit',
+			propers: {
+				'universal-gloria-disabled': true,
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+			}
+		},
+
+		Vespers: {
+			order: 'vespers/triduum-order.lit',
+			psalter: 'vespers/triduum.lit',
+			propers: {
+				magnificat: 'propers/triduum/friday/vespers/magnificat.lit',
+				'universal-gloria-disabled': true,
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+			}
+		},
+
+		Compline: {
+			order: 'terce/triduum-order.lit',
+			psalter: 'compline/triduum.lit',
+			propers: {
+				'universal-gloria-disabled': true,
+				preces: 'propers/triduum/friday/tenebrae/preces.lit',
+			}
+		}
+	}
+
 	// TODO: pascha
 	// TODO: ascension
 	// TODO: pentecost
