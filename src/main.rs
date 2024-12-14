1c3eee7d4407004e2e0c6dd33badd7f5138ec78f
Daniel Write <daniel@writefamily.com>
2024-12-14 03:24:30 -0500

    added CORS rules

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,15 @@ fn main() {
     println!("http://127.0.0.1:{}", bind_addr.port());
 
     rouille::start_server(bind_addr.to_string(), move |request| {
-        match request.url().as_str() {
+        if request.method() == "OPTIONS" {
+            return rouille::Response::text("")
+                .with_status_code(204)
+                .with_additional_header("Access-Control-Allow-Origin", "*")
+                .with_additional_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+                .with_additional_header("Access-Control-Allow-Headers", "*");
+        }
+
+        let mut response = match request.url().as_str() {
             "/liturgy.css" => router::static_file("public/liturgy.css", "text/css"),
             "/suncalc.js" => router::static_file("public/suncalc.js", "application/javascript"),
             "/lit-time.js" => router::static_file("public/lit-time.js", "application/javascript"),
@@ -27,6 +35,13 @@ fn main() {
             "/exsurge.min.js.map" => router::static_file("public/exsurge.min.js.map", "application/javascript"),
             "/" => router::static_file("public/index.html", "text/html"),
             _ => router::dynamic(request),
-        }
+        };
+
+        response = response
+            .with_additional_header("Access-Control-Allow-Origin", "*")
+            .with_additional_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+            .with_additional_header("Access-Control-Allow-Headers", "*");
+
+        response
     });
 }
