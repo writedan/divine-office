74b76172cb4ed4ae86c0f0b7e533d112f1ac980c
Daniel Write <daniel@writefamily.com>
2025-02-23 23:00:44 -0500

    Added ability to compile to Lua runtime

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -8,6 +8,7 @@ mod timehelp;
 mod wasm;
 
 use wasm_bindgen::prelude::*;
+use mlua::prelude::*;
 
 #[wasm_bindgen]
 pub fn get_identifier(year: i32, month: u32, day: u32) -> String {
@@ -31,4 +32,24 @@ pub fn get_hour(year: i32, month: u32, day: u32, hour: &str) -> String {
         Ok(elements) => serde_json::to_string(&elements).unwrap(),
         Err(err) => serde_json::to_string(&err).unwrap(),
     }
+}
+
+#[cfg(feature = "lua_support")]
+#[mlua::lua_module]
+fn divine_office(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    
+    exports.set("get_identifier", lua.create_function(|_, (year, month, day): (i32, u32, u32)| {
+        Ok(get_identifier(year, month, day))
+    })?)?;
+    
+    exports.set("get_monthly_identifiers", lua.create_function(|_, (year, month): (i32, u32)| {
+        Ok(get_monthly_identifiers(year, month))
+    })?)?;
+    
+    exports.set("get_hour", lua.create_function(|_, (year, month, day, hour): (i32, u32, u32, String)| {
+        Ok(get_hour(year, month, day, &hour))
+    })?)?;
+    
+    Ok(exports)
 }
