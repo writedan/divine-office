bc6186858e643b6d387ecf6908c76983a619676d
Daniel Write <daniel@writefamily.com>
2025-12-01 14:59:17 -0500

    Added documentation for asset.rs

--- backend/src/asset.rs
+++ backend/src/asset.rs
@@ -8,4 +8,5 @@ use rust_embed::Embed;
 #[exclude = "target/*"]
 #[exclude = "Cargo.toml"]
 #[exclude = ".git/*"]
+/// This is used to embed all the liturgical files into the application itself so that no IO operations are ever used. This allows us to embed directly into WASM so that the frontend can run without a dedicated server.
 pub struct Asset;
